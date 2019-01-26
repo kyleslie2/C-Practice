879973b1f5eab503a72449a3e816b7d588b6e5f0
Kyle Leslie <31903847+kyleslie2@users.noreply.github.com>
2019-01-26 11:08:41 -0500

    Add files via upload

---
+++ 8 MVC layout, Razor syntax, View-model objects/Lab7/Models/DataAccess/AcademicRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab7.Models.DataAccess
+{
+    public partial class AcademicRecord
+    {
+        public string CourseCode { get; set; }
+        public string StudentId { get; set; }
+        public int? Grade { get; set; }
+
+        public Course CourseCodeNavigation { get; set; }
+        public Student Student { get; set; }
+    }
+}
